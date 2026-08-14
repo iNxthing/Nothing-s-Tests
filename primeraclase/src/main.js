d768552a07e9434ebdb3d8dc485ca7879fa58182
Darwin Garcia M. <darwingarciam1553@gmail.com>
2026-08-13 20:55:58 -0500

    My first commit

--- primeraclase/src/main.js
+++ primeraclase/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import MenuComponent from '@/components/MenuComponent.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App).use(router);
+app.component("MenuComponent",MenuComponent);
+app.mount('#app');
+
+export {app};
