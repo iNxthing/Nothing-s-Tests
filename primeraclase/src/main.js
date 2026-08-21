ebe8596c4ea4971c27f40b42fb396a7a0a9a85cf
Darwin Garcia M. <darwingarciam1553@gmail.com>
2026-08-20 19:20:20 -0500

    Nueva vista en la pagina

--- primeraclase/src/main.js
+++ primeraclase/src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import MenuComponent from '@/components/MenuComponent.vue';
-
+import MessageComponent from './components/MessageComponent.vue';
 const app = createApp(App).use(router).use(router);
 app.component("MenuComponent",MenuComponent);
+app.component("MessageComponent",MessageComponent);
 app.mount('#app');
 
 export {app};
--- primeraclase/src/router/index.js
+++ primeraclase/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Contact from '@/views/ContactUs.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/contactus',
+    name: 'ContactUs',
+    component: Contact
   },
   {
     path: '/about',
